880e11a7c83814aeea204f8f49db01f177b3d8b4
abreaux26 <angelbreaux26@gmail.com>
2021-04-05 11:29:50 -0500

    Implement facade in controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,6 @@
 class SearchController < ApplicationController
   def index
-    conn = Faraday.new(url: "https://last-airbender-api.herokuapp.com/")
-    response = conn.get("/api/v1/characters?affiliation=Fire+Nation&perPage=100")
-
-    @fire_nation_characters = JSON.parse(response.body, symbolize_names: true)
-    @limit_characters = @fire_nation_characters.first(25)
+    @fire_nation_characters = SearchFacade.get_affiliate_members("Fire Nation")
+    @limit_characters = SearchFacade.search("Fire Nation", 25)
   end
 end
