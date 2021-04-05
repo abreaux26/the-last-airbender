b4693b36706f52afa72182a91be05ce65f748c93
abreaux26 <angelbreaux26@gmail.com>
2021-04-05 10:57:02 -0500

    Create faraday connection

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,8 @@
 class SearchController < ApplicationController
   def index
+    conn = Faraday.new(url: "https://last-airbender-api.herokuapp.com/")
+    response = conn.get("/api/v1/characters?affiliation=Fire+Nation")
+
+    @fire_nation_characters = JSON.parse(response.body, symbolize_names: true)
   end
 end
