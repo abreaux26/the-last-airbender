a256c89ce91d56907d8edd014ba2a25e183f5a5a
abreaux26 <angelbreaux26@gmail.com>
2021-04-05 11:40:33 -0500

    Update service spec and create service

--- spec/services/affiliation_service_spec.rb
+++ spec/services/affiliation_service_spec.rb
@@ -6,12 +6,12 @@ RSpec.describe AffiliationService do
       affiliation = 'Fire Nation'
       limit = 25
 
-      data = AffiliationService.search(name, limit)
+      data = AffiliationService.search(affiliation, limit)
       expect(data).to be_a(Array)
       expect(data.length).to be <= limit
 
       first_data = data[0]
-      expect(first_data).to have_key(:id)
+      expect(first_data).to have_key(:_id)
       expect(first_data).to have_key(:name)
       expect(first_data).to have_key(:allies)
       expect(first_data).to have_key(:enemies)
@@ -31,7 +31,7 @@ RSpec.describe AffiliationService do
       expect(data[0]).to have_key(:enemies)
       expect(data[0][:enemies]).to be_an(Array)
       expect(data[0]).to have_key(:name)
-      expect(data[0][:allies]).to be_a(String)
+      expect(data[0][:name]).to be_a(String)
       expect(data[0]).to have_key(:affiliation)
       expect(data[0][:affiliation]).to be_an(String)
     end
